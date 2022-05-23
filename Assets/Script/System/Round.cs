b50da8af608cbc83de9ffa22148ead6aef16638c
ZeroFe <skel2cha@gmail.com>
2022-05-23 17:53:04 +0900

    게임 맵 제작 라운드 시스템 개선중

--- Assets/Script/System/Round.cs
+++ Assets/Script/System/Round.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// 라운드에 나올 몬스터 수 지정 및 나오는 타이밍 등 세부 정보를 담은 데이터
@@ -10,29 +11,41 @@ using UnityEngine;
 public class Round
 {
     // 특정 몬스터에 대한 스폰 정보
+    // 어떤 경로로, 어떤 몬스터가, 얼마나 생성될지
     [System.Serializable]
-    public class EnemySpawnInfo
+    public class EnemySpawn
     {
         public enum SpawnType
         {
-            // 하나씩,
-            // 한 번에,
+            Sequence,
+            Emission,
         }
 
-        public GameObject enemyPrefab;
-        public int spawnCount = 10;
+        public GameObject routeObject;
 
-        public float spawnInitTime = 0.0f;
-        public float spawnBaseTime = 3.0f;
-        public AnimationCurve spawnTimeScaleCurve;
+        public SpawnType spawnType = SpawnType.Sequence;
+        public GameObject enemyPrefab;
+        public int count = 10;
+
+        [Tooltip("언제부터 나올지")]
+        public float startTime = 0.0f;
+        [Header("Sequence Setting")]
+        [Tooltip("몬스터가 나오는 주기")]
+        public float period = 3.0f;
+        public AnimationCurve periodScaleCurve;
+        [Header("For Spawn")]
+        // 디버그용으로 public으로 냅두고, 이후 HideInspector를 적용해서 안 보이게 한다
+        public float spawnWaitTime = 0.0f;
+        public int remainCount = 10;
     }
 
-    public List<EnemySpawnInfo> enemies = new List<EnemySpawnInfo>();
+    public List<EnemySpawn> enemySpawns = new List<EnemySpawn>();
 
     [Header("Round Power Up Setting")]
     
     public float enemyPowerupRate = 1.2f;
     // 돌연변이 확률
+    //public float enemyMutateChance = 0.0f;
 
 
 }
--- Assets/Script/System/RoundSystem.cs
+++ Assets/Script/System/RoundSystem.cs
@@ -1,50 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
+// 디버그를 위해 의도적으로 DisallowMultipleComponent는 넣지 않는다
+// 여러 디버그용 Round를 제작해야할 수도 있기 때문
 public class RoundSystem : Singleton<RoundSystem>
 {
     private static readonly float SPAWN_CHECK_TIME = 0.1f;
-
-    public int enemySpawnStartCount = 20;
-    // 라운드마다 증가한다
-    public int enemySpawnIncreaseCount = 5;
     
-    private int currentEnemyCount;
+    public List<Round> rounds = new List<Round>();
+    private int currentRoundIndex = -1;
+    public Round CurrentRound
+    {
+        get;
+        private set;
+    }
+
     private int remainEnemyCount;
-
-    [SerializeField] 
-    private AnimationCurve spawnTimeCurve;
-    [SerializeField] 
-    private float spawnBaseTime = 5.0f;
-
-    private float spawnWaitTime;
-
-    // 임시 코드 : 지수승 방식 몬스터 강화
-    [SerializeField] private float enemyPowerupRate = 1.2f;
-    private float currentEnemyPowerup = 1.0f;
-
-    [SerializeField]
-    private int MaxRound = 10;
-    private int currentRound = 0;
-
-    [Header("Enemy Prefab")] 
-    [SerializeField]
-    private GameObject[] enemyPrefabs;
-
-    [SerializeField]
-    private Transform spawnTr;
 
     [Header("UI")]
     public GameObject GameClearUI;
 
     public void Start()
     {
-        // 임시 코드 : 첫 스테이지는 강화하지 않게 처리
-        currentEnemyPowerup /= enemyPowerupRate;
-
         // 원래는 게임 시작 시 무기를 선택한 후, 라운드를 시작해야 함
         // 일단 간이 테스트로 시작하자마자 라운드 진행
         NextRound();
@@ -58,14 +37,14 @@ public class RoundSystem : Singleton<RoundSystem>
     public void CheckRoundEnd()
     {
         // 몹이 음수 개만큼 존재해선 안 된다
-        Debug.Assert(remainEnemyCount >= 0 && currentEnemyCount >= 0);
+        //Debug.Assert(remainEnemyCount >= 0 && current >= 0);
 
-        currentEnemyCount--;
-        Debug.Log("current enemy Count : " + currentEnemyCount);
-        if (remainEnemyCount == 0 && currentEnemyCount == 0)
+        remainEnemyCount--;
+        Debug.Log("current enemy Count : " + remainEnemyCount);
+        if (remainEnemyCount == 0 && CurrentRound.enemySpawns.Count == 0)
         {
             // 게임 끝났나 체크
-            if (currentRound == MaxRound)
+            if (currentRoundIndex == rounds.Count)
             {
                 // 게임 끝났으면 게임 완료 처리
                 GameClearUI.SetActive(true);
@@ -73,6 +52,8 @@ public class RoundSystem : Singleton<RoundSystem>
             // 아니면 업그레이드 띄우기
             else
             {
+                // 라운드 끝 애니메이션
+                // 업그레이드 할 수 있다 띄우고 윈도우 띄우기
                 WindowSystem.Instance.OpenWindow(UpgradeWindow.Instance.gameObject, false);
                 UpgradeWindow.Instance.SetUpgradeWindow(EnhanceSystem.Instance.GetRandomEnhances(3));
             }
@@ -82,53 +63,79 @@ public class RoundSystem : Singleton<RoundSystem>
     // 다음 라운드 시작
     public void NextRound()
     {
-        currentRound++;
+        // 라운드 시작 애니메이션
 
-        remainEnemyCount = enemySpawnStartCount + currentRound * enemySpawnIncreaseCount;
-        spawnWaitTime = spawnBaseTime;
 
-        // 임시 강화
-        currentEnemyPowerup *= enemyPowerupRate;
+        currentRoundIndex++;
+        // 리스트에서 라운드 정보를 읽고 시작한다
+        CurrentRound = rounds[currentRoundIndex];
 
-        StartCoroutine(SpawnMonster());
+        StartCoroutine(SpawnMonsters());
     }
 
-    private IEnumerator SpawnMonster()
+    private IEnumerator SpawnMonsters()
     {
-        int totalSpawnMonster = remainEnemyCount;
-        while (remainEnemyCount > 0)
+        var enemySpawns = CurrentRound.enemySpawns;
+        // 처음 시작할 때 spawn Wait Time을 시작 시간으로 맞춘다
+        foreach (var spawn in enemySpawns)
         {
-            if (spawnWaitTime <= 0)
+            spawn.spawnWaitTime = spawn.startTime;
+            spawn.remainCount = spawn.count;
+        }
+
+        // 
+        while (CurrentRound.enemySpawns.Count > 0)
+        {
+            // 남은 몬스터가 없는 라운드는 삭제해야 한다
+            // 정방향 순회는 Remove를 사용했을 때 오류가 생길 가능성이 있으므로 역방향 순회를 사용한다
+            for (int i = CurrentRound.enemySpawns.Count - 1; i >= 0; i--)
             {
-                // 초기엔 몬스터 스폰 간격이 넓어야 한다
-                // 이후 마지막에 가까워질수록 스폰 시간을 줄인다
-                // => 스폰 간격을 커브 식으로 조정
-                spawnWaitTime = spawnTimeCurve.Evaluate((float)remainEnemyCount / totalSpawnMonster) * spawnBaseTime;
-                Debug.Log("spawn wait time : " + spawnWaitTime);
-                remainEnemyCount--;
-                // 몬스터 스폰
-                Spawn();
+                var currEnemySpawn = enemySpawns[i];
+                if (currEnemySpawn.spawnWaitTime <= 0)
+                {
+                    // 순차적으로 스폰
+                    if (currEnemySpawn.spawnType == Round.EnemySpawn.SpawnType.Sequence)
+                    {
+                        // 스폰 코드
+                        currEnemySpawn.spawnWaitTime = 
+                            currEnemySpawn.periodScaleCurve.Evaluate((float)currEnemySpawn.remainCount / currEnemySpawn.count) * 
+                            currEnemySpawn.period;
+                    }
+                    // 한 번에 스폰
+                    else if (currEnemySpawn.spawnType == Round.EnemySpawn.SpawnType.Emission)
+                    {
+                        // for문 스폰 코드
+                        currEnemySpawn.count = 0;
+                    }
+                }
+
+                if (currEnemySpawn.remainCount == 0)
+                {
+                    enemySpawns.RemoveAt(i);
+                }
+                else
+                {
+                    currEnemySpawn.spawnWaitTime -= SPAWN_CHECK_TIME;
+                }
             }
 
             // 몬스터를 처치하면 스폰 간격 좀 줄인다 - 보류
-
             yield return new WaitForSeconds(SPAWN_CHECK_TIME);
-            spawnWaitTime -= SPAWN_CHECK_TIME;
         }
 
         Debug.Log("create end");
     }
 
-    private void Spawn()
-    {
-        // 중간 중간 설정한 몬스터가 나와야 한다
-        Debug.Log("Monster Spawn!!!");
-        int randNum = UnityEngine.Random.Range(0, enemyPrefabs.Length);
-        var go = Instantiate(enemyPrefabs[randNum], spawnTr.position, Quaternion.identity);
-        go.GetComponent<EnemyStatus>().PowerUp(currentEnemyPowerup);
-        EnemyManager.Instance.SetEnemyTarget(go);
-        currentEnemyCount++;
-    }
+    //private void Spawn()
+    //{
+    //    // 중간 중간 설정한 몬스터가 나와야 한다
+    //    Debug.Log("Monster Spawn!!!");
+    //    int randNum = UnityEngine.Random.Range(0, enemyPrefabs.Length);
+    //    var go = Instantiate(enemyPrefabs[randNum], spawnTr.position, Quaternion.identity);
+    //    go.GetComponent<EnemyStatus>().PowerUp(currentEnemyPowerup);
+    //    EnemyManager.Instance.SetEnemyTarget(go);
+    //    currentEnemyCount++;
+    //}
 
     #region Debug
 
